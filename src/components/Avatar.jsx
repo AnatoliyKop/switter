221db492cf0ff55beb4368ba06c680046aee78ff
AnatoliyKop <ttolea90tolea132@gmail.com>
2025-03-27 00:03:29 +0100

    changeAvatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,10 +1,21 @@
 import React, {useContext} from 'react';
 import {TwitterContext} from "../utils/context.js";
 
+
+
+
 const Avatar = ({size}) => {
-    const {user} = useContext(TwitterContext)
+    const {user,setUser} = useContext(TwitterContext)
+
+    const changeAvatar= ()=>{
+        const newAvatar=prompt("Enter new avatar name")
+    {setUser({ ...user,avatar:`https://gravatar.com/avatar/000?d=${newAvatar}`})}
+
+
+
+    }
     return (
-            <img className={`user-avatar ${size|| ""}`} src={user.avatar} alt={user.name}/>
+            <img onClick={changeAvatar} className={`user-avatar ${size|| ""}`} src={user.avatar} alt={user.name}/>
 
     );
 };
