e1acbc82e4f9bc5094229587b1d7a921d9ed2156
AnatoliyKop <ttolea90tolea132@gmail.com>
2025-03-26 17:37:12 +0100

    AddTwittercontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 import Navigation from "./components/Navigation.jsx";
 import Body from "./components/Body.jsx";
+import {TwitterContext} from "./utils/context.js";
 
 function App() {
     const [user, setUser] = useState({
         name: "Monster ",
-        avatar:'https://gravatar.com/avatar/000?d=mp'
+        avatar: 'https://gravatar.com/avatar/000?d=mp'
     });
     const [stats, setStats] = useState({
-        followers:10,
+        followers: 10,
         following: 20
     })
 
-  return (
-    <div className={"app"}>
-      <Navigation user={user}  />
-        <Body user={user} stats={stats} />
-    </div>
-  )
+    return (
+        <div className={"app"}>
+            <TwitterContext.Provider value={{
+                 user, stats
+            }}>
+            <Navigation/>
+            <Body />
+            </TwitterContext.Provider>
+        </div>
+    )
 }
 
 export default App
